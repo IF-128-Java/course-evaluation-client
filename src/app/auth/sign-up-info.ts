d79a4797bfe1ba9db37d340da0f06cce79cd3f89
Vova <51489244+VovaProtsiv@users.noreply.github.com>
2021-07-25 12:50:38 +0300

    mapped SignUpInfo with registers form

--- src/app/auth/sign-up-info.ts
+++ src/app/auth/sign-up-info.ts
@@ -5,11 +5,12 @@ export class SignUpInfo {
   password: string;
   role: string[];
 
-  constructor(firstName: string, lastName: string, email: string, password: string, role: string[]) {
+
+  constructor(firstName: string, lastName: string, email: string, password: string) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.email = email;
     this.password = password;
-    this.role = role;
+    this.role = ['ROLE_STUDENT'];
   }
 }
