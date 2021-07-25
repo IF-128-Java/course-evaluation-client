574b266311b9063b782050b740120110cba9a9aa
Vova <51489244+VovaProtsiv@users.noreply.github.com>
2021-07-25 09:53:19 +0300

    added auth.service

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {AuthLoginInfo} from "../auth/auth-login-info";
+import {SignUpInfo} from "../auth/sign-up-info";
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private loginUrl = 'http://localhost:8080/api/v1/auth/signin';
+  private signupUrl = 'http://localhost:8080/api/v1/auth/signup';
+
+  constructor(private http: HttpClient) {
+  }
+
+  attemptAuth(credentials: AuthLoginInfo): Observable<any> {
+    return this.http.post(this.loginUrl, credentials, httpOptions);
+  }
+
+  signUp(info: SignUpInfo): Observable<any> {
+    return this.http.post(this.signupUrl, info, httpOptions);
+  }
+}
