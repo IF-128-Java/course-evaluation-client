60f28522631d79557c9b17507f75c78bf0740894
Vova <51489244+VovaProtsiv@users.noreply.github.com>
2021-07-25 09:16:57 +0300

    added auth-interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import {AppComponent} from './app.component';
+import { AppComponent } from './app.component';
 import {RouterModule} from "@angular/router";
 import {AppRoutingModule} from './app-routing.module';
 import {HomeComponent} from './components/home/home.component';
