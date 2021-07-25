a9ba5868aa97751eeec6ab18b6bb4e801c3dac7e
Vova <51489244+VovaProtsiv@users.noreply.github.com>
2021-07-25 21:50:04 +0300

    added base roles check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TokenStorageService} from "./auth/token-storage.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'course-evaluation-client';
+   roles: string[] | undefined;
+
+  constructor(private tokenStorage: TokenStorageService) {
+  }
+
+  ngOnInit() {
+    if (this.tokenStorage.getToken()) {
+      this.roles = this.tokenStorage.getAuthorities();
+
+    }
+  }
 }
