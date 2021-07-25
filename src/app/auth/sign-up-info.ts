df645a0d96396f50153f476f46089ad461e9f3d7
Vova <51489244+VovaProtsiv@users.noreply.github.com>
2021-07-25 09:45:07 +0300

    added sign-up-info.ts

---
+++ src/app/auth/sign-up-info.ts
@@ -0,0 +1,15 @@
+export class SignUpInfo {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  role: string[];
+
+  constructor(firstName: string, lastName: string, email: string, password: string, role: string[]) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.password = password;
+    this.role = role;
+  }
+}
