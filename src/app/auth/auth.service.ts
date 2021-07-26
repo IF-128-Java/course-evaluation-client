dc5b4edb519f6945073442a67a1ee8c947b5fbdf
Vova <51489244+VovaProtsiv@users.noreply.github.com>
2021-07-26 20:43:50 +0300

    defined constants in AppConfig

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,13 +1,14 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import {AuthLoginInfo} from "./auth-login-info";
-import {SignUpInfo} from "./sign-up-info";
-import {JwtHelperService} from "@auth0/angular-jwt";
-import {TokenStorageService} from "./token-storage.service";
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {AuthLoginInfo} from './auth-login-info';
+import {SignUpInfo} from './sign-up-info';
+import {JwtHelperService} from '@auth0/angular-jwt';
+import {TokenStorageService} from './token-storage.service';
+import {AppConfig} from '../common/app-config';
 
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
 };
 
 @Injectable({
@@ -15,10 +16,10 @@ const httpOptions = {
 })
 export class AuthService {
 
-  private loginUrl = 'http://localhost:8080/api/v1/auth/signin';
-  private signupUrl = 'http://localhost:8080/api/v1/auth/signup';
+  private loginUrl = AppConfig.API_ENDPOINT + 'auth/login';
+  private signupUrl = AppConfig.API_ENDPOINT + 'auth/signup';
 
-  constructor(private http: HttpClient, private jwtHelper: JwtHelperService,private tokenStorage: TokenStorageService) {
+  constructor(private http: HttpClient, private jwtHelper: JwtHelperService, private tokenStorage: TokenStorageService) {
   }
 
   attemptAuth(credentials: AuthLoginInfo): Observable<any> {
@@ -29,7 +30,7 @@ export class AuthService {
     return this.http.post(this.signupUrl, info, httpOptions);
   }
 
- public isAuthenticated(): boolean {
+  public isAuthenticated(): boolean {
     const token = this.tokenStorage.getToken();
     return !this.jwtHelper.isTokenExpired(token);
   }
