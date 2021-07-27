b0e1624aa4628bfe62263c2e15a9dd03b7770d36
Vova <51489244+VovaProtsiv@users.noreply.github.com>
2021-07-27 20:52:21 +0300

    changed Bearer_ to Bearer

--- src/app/common/app-config.ts
+++ src/app/common/app-config.ts
@@ -1,7 +1,7 @@
 export class AppConfig {
   public static API_ENDPOINT='http://localhost:8080/api/v1/';
   public static TOKEN_HEADER_KEY = 'Authorization';
-  public static AUTHENTICATION_SCHEME='Bearer_';
+  public static AUTHENTICATION_SCHEME='Bearer ';
 
   public static TOKEN_KEY = 'AuthToken';
   public static USERNAME_KEY = 'AuthUsername';
