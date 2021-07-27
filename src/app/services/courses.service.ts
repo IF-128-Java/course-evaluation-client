df49e92b5a9635e3915449444be3928574ad2de4
james <jamessss.if@gmail.com>
2021-07-27 23:26:42 +0300

    Added app-config

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Course} from "../models/course.model";
+import {AppConfig} from "../common/app-config";
 
-const baseUrl = 'http://localhost:8080/courses/';
+// const baseUrl = 'http://localhost:8080/courses/';
 
 @Injectable({
   providedIn: 'root'
@@ -13,26 +14,26 @@ export class CoursesService {
   constructor(private  http: HttpClient) { }
 
   getAll(): Observable<Course[]> {
-    return this.http.get<Course[]>(baseUrl);
+    return this.http.get<Course[]>(AppConfig.API_ENDPOINT);
   }
 
   get(id: number): Observable<Course> {
-    return this.http.get(`${baseUrl}/${id}`);
+    return this.http.get(`${AppConfig.API_ENDPOINT}/${id}`);
   }
 
   create(data: any): Observable<any> {
-    return this.http.post(baseUrl, data);
+    return this.http.post(AppConfig.API_ENDPOINT, data);
   }
 
   update(id: number | any, data: any): Observable<any> {
-    return this.http.put(`${baseUrl}/${id}`, data);
+    return this.http.put(`${AppConfig.API_ENDPOINT}/${id}`, data);
   }
 
   delete(id: number | any): Observable<Course> {
-    return this.http.delete(baseUrl);
+    return this.http.delete(AppConfig.API_ENDPOINT);
   }
 
   findByCourseName(courseName: string): Observable<Course[]> {
-    return this.http.get<Course[]>(`${baseUrl}?courseName=${courseName}`);
+    return this.http.get<Course[]>(`${AppConfig.API_ENDPOINT}?courseName=${courseName}`);
   }
 }
