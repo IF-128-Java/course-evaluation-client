579d987495c17e68ed7af941530c852327263ebb
james <jamessss.if@gmail.com>
2021-07-27 08:13:14 +0300

    Courses-list and course-details were making

--- src/app/components/course-details/course-details.component.ts
+++ src/app/components/course-details/course-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Course} from "../../models/course.model";
+import {CoursesService} from "../../services/courses.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-course-details',
@@ -7,9 +10,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseDetailsComponent implements OnInit {
 
-  constructor() { }
+  currentCourse: Course = {
+    courseName: '',
+    description: '',
+    startDate: '',
+    endDate: '',
+    teacher: ''
+  };
+  message = ';'
+
+  constructor(
+    private coursesService: CoursesService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.message = '';
+    this.getCourse(this.route.snapshot.params.id);
   }
 
+  getCourse(id: number): void {
+    this.coursesService.get(id).subscribe(data => {
+      this.currentCourse = data;
+      console.log(data);
+      },
+      error => {
+      console.log(error);
+      }
+    );
+  }
+
+  UpdateCourse(): void {
+    this.message = '';
+
+    this.coursesService.update(this.currentCourse.id, this.currentCourse).subscribe(
+      response => { console.log(response);
+      this.message = response.message ? response.message : 'Course was updated!'; },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  deleteCourse(): void {
+    this.coursesService.delete(this.currentCourse.id).subscribe(
+      response => {
+        console.log(response);
+        this.router.navigate(['/courses']);
+      },
+      error => {
+        console.log(error);
+      }
+    )
+  }
 }
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -3,7 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Course} from "../models/course.model";
 
-const baseUrl = 'http://localhost:8080/courses';
+const baseUrl = 'http://localhost:8080/courses/';
 
 @Injectable({
   providedIn: 'root'
@@ -24,11 +24,11 @@ export class CoursesService {
     return this.http.post(baseUrl, data);
   }
 
-  update(id: number, data: any): Observable<any> {
+  update(id: number | any, data: any): Observable<any> {
     return this.http.put(`${baseUrl}/${id}`, data);
   }
 
-  delete(id: number): Observable<Course> {
+  delete(id: number | any): Observable<Course> {
     return this.http.delete(baseUrl);
   }
 
