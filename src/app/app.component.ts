5496b4bd4008214db56361198a2debe5e1ac8244
Vova <51489244+VovaProtsiv@users.noreply.github.com>
2021-07-27 21:00:25 +0300

    added users list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TokenStorageService} from "./auth/token-storage.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'course-evaluation-client';
+  roles: string[] | undefined;
+  username: string | undefined;
+  navbarOpen = false;
+
+  constructor(private tokenStorage: TokenStorageService) {
+  }
+
+  ngOnInit() {
+    if (this.tokenStorage.getToken()) {
+      this.roles = this.tokenStorage.getAuthorities();
+      this.username = this.tokenStorage.getUsername();
+    }
+  }
+  logout() {
+    this.tokenStorage.signOut();
+    window.location.reload();
+  }
+
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
 }
