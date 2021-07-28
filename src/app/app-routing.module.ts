f9734d0ad19c5de5d8ba17b4b26ce393fb1b46b0
Vova <51489244+VovaProtsiv@users.noreply.github.com>
2021-07-28 14:21:28 +0300

    added groups list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import {LoginComponent} from './components/login/login.component';
 import {RegisterComponent} from './components/register/register.component';
 import {AdminComponent} from './components/admin/admin.component';
 import {AuthGuardService} from './auth/auth-guard.service';
+import {GroupsListComponent} from './components/groups-list/groups-list.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'admin', component: AdminComponent,canActivate:[AuthGuardService]},
+  {path: 'groups-list', component: GroupsListComponent,canActivate:[AuthGuardService]},
   {path: '', redirectTo: 'home', pathMatch: 'full'}
 ];
 
