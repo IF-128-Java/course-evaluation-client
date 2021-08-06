160316502da237e4e6276bb0b9ed0bf0745bc905
Vova <51489244+VovaProtsiv@users.noreply.github.com>
2021-08-06 16:28:04 +0300

    changed group-list

--- src/app/admin_project/components/group/groups-list/groups-list.component.ts
+++ src/app/admin_project/components/group/groups-list/groups-list.component.ts
@@ -1,72 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import {Group} from '../../../../models/group.model';
-import {GroupService} from '../../../../admin_project/services/group.service';
-import {NgbActiveModal, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {GroupDto} from '../../../models/group-dto.model';
+import {GroupService} from "../../../services/group.service";
+import {MatDialog, MatDialogRef} from "@angular/material/dialog";
+import {UpdateRoleComponent} from "../../user/update-role/update-role.component";
+import {StudentListComponent} from "../student-list/student-list.component";
 
-@Component({
-  selector: 'ngbd-modal-content',
-  template: `
-    <div class="modal-header">
-      <h4 class="modal-title">Create new group</h4>
-      <button type="button" class="close" aria-label="Close" (click)="activeModal.dismiss('Cross click');;refresh()">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
-    <div class="modal-body">
-      <form name="form" (ngSubmit)="f.form.valid && onSubmit()" #f="ngForm" novalidate>
-        <div class="form-group">
-          <label>Group name</label>
-          <input
-            type="text"
-            class="form-control"
-            name="groupName"
-            [(ngModel)]="form.groupName" #groupName="ngModel" required
-          />
-          <div *ngIf="groupName.errors">Group name is required</div>
-          <div *ngIf="isSignedUp && group">Group {{group.groupName}} has added!</div>
-        </div>
-        <div class="form-group">
-          <button class="btn btn-primary btn-block">Add</button>
-        </div>
-      </form>
-    </div>
-    <div class="modal-footer">
-      <button type="button" class="btn btn-outline-dark" (click)="activeModal.close('Close click');refresh()">Close
-      </button>
-    </div>
-  `
-})
-export class NgbdModalContent {
-  form: any = {};
-  group: Group | undefined;
-  isSignedUp = false;
-  isSignUpFailed = false;
-  errorMessage = '';
-
-  constructor(private groupService: GroupService, public activeModal: NgbActiveModal) {
-  }
-
-  onSubmit() {
-    this.group = new Group('', this.form.groupName);
-
-    this.groupService.create(this.group).subscribe(
-      data => {
-        console.log(data);
-        this.isSignedUp = true;
-        this.isSignUpFailed = false;
-      },
-      error => {
-        console.log(error);
-        this.errorMessage = error.error.message;
-        this.isSignUpFailed = true;
-      }
-    );
-  }
-
-  public refresh() {
-    window.location.reload();
-  }
-}
 
 @Component({
   selector: 'app-groups-list',
@@ -74,29 +12,27 @@ export class NgbdModalContent {
   styleUrls: ['./groups-list.component.css']
 })
 export class GroupsListComponent implements OnInit {
-
-  groups: Group[] = [];
-
-  constructor(private groupService: GroupService, private modalService: NgbModal) {
+  public groups: GroupDto[]=[];
+  public displayedColumns: string[] = ['Id', 'Group Name','Students','Courses'];
+  constructor(private groupService:GroupService, public dialog: MatDialog, public dialModRef:MatDialogRef<any>) {
+  }
+  ngOnInit(): void {
+    this.groupService.getGroups().subscribe(data=>{
+      this.groups=data;
+    })
   }
 
-  ngOnInit() {
-    this.groupService.getGroups().subscribe(
-      data => {
-        this.groups = data;
+
+  openStudents(id: number) {
+    this.groupService.getById(id).subscribe(data => {
+        const dialogRef = this.dialog.open(StudentListComponent, {
+          width: '400px',
+          data: data
+        });
+        dialogRef.afterClosed().subscribe(result => {
+          window.location.reload();
+        });
       }
     );
   }
-
-  deleteRoom(group: Group) {
-    this.groupService.delete(group).subscribe(d => {
-      this.groups = this.groups.filter(h => h != group)
-    });
-  }
-
-  open() {
-    const modalRef = this.modalService.open(NgbdModalContent);
-    modalRef.componentInstance.name = 'World';
-  }
-
 }
