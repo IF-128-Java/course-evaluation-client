03dc177d8f6ea69cdf99bdcafab7724e0b14f6dc
Vova <51489244+VovaProtsiv@users.noreply.github.com>
2021-08-06 16:31:47 +0300

    added get user by id

--- src/app/admin_project/services/group.service.ts
+++ src/app/admin_project/services/group.service.ts
@@ -3,13 +3,13 @@ import {AppConfig} from '../../common/app-config';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Group} from '../../models/group.model';
+import {GroupDto} from '../models/group-dto.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GroupService {
-private group =new Group('4','test');
-  private groupUrl = AppConfig.API_ENDPOINT + 'groups';
+  private groupUrl = AppConfig.API_ADMIN_ENDPOINT + 'groups';
 
   constructor(private http: HttpClient) {
 
@@ -19,12 +19,16 @@ private group =new Group('4','test');
     return this.http.get(this.groupUrl);
   }
 
-  public delete(group: Group) :Observable<any>{
-    return this.http.delete(this.groupUrl+'/'+group.id)
+  getById(id: number): Observable<GroupDto> {
+    return this.http.get(this.groupUrl + '/' + id);
+  }
+
+  public delete(group: Group): Observable<any> {
+    return this.http.delete(this.groupUrl + '/' + group.id)
   }
 
   create(group: Group) {
-    return this.http.post(this.groupUrl,group)
+    return this.http.post(this.groupUrl, group)
   }
 
 }
