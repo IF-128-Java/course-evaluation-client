88e9ba0af44eb71890c9641c046ad459cd02b27a
james <jamessss.if@gmail.com>
2021-08-18 22:46:40 +0300

    create course admin side

---
+++ src/app/admin_project/components/course/admin-create-course/admin-create-course.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-create-course',
+  templateUrl: './admin-create-course.component.html',
+  styleUrls: ['./admin-create-course.component.css']
+})
+export class AdminCreateCourseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
