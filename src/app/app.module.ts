90faf6325e4b05fe05a2fd036acfda4c50808727
james <jamessss.if@gmail.com>
2021-08-18 22:46:40 +0300

    create course admin side

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -51,6 +51,7 @@ import {PassedCoursesComponent} from './components/student/Passed-courses/passed
 import {CurrentCoursesComponent} from './components/student/Current-courses/current-courses.component';
 import {AvCoursesComponent} from './components/student/Av-courses/av-courses.component';
 import { AdminCourseListComponent } from './admin_project/components/course/admin-course-list/admin-course-list.component';
+import { AdminCreateCourseComponent } from './admin_project/components/course/admin-create-course/admin-create-course.component';
 
 @NgModule({
   declarations: [
@@ -76,7 +77,8 @@ import { AdminCourseListComponent } from './admin_project/components/course/admi
     PassedCoursesComponent,
     CurrentCoursesComponent,
     AvCoursesComponent,
-    AdminCourseListComponent
+    AdminCourseListComponent,
+    AdminCreateCourseComponent
   ],
   imports: [
     BrowserModule,
