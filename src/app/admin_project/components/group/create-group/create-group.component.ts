fa38fe519154d99cfdbf716020a46ce08e06d419
Vova <51489244+VovaProtsiv@users.noreply.github.com>
2021-08-19 15:28:59 +0300

    deleted home works and group from toolbar

--- src/app/admin_project/components/group/create-group/create-group.component.ts
+++ src/app/admin_project/components/group/create-group/create-group.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {GroupDto} from '../../../models/group-dto.model';
 import {GroupService} from '../../../services/group.service';
-import {MatDialogRef} from "@angular/material/dialog";
+import {MatDialogRef} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-create-group',
