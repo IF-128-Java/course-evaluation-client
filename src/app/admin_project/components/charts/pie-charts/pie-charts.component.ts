3fcc216530ab876a8feb1e6e60212222fb85bee9
james <jamessss.if@gmail.com>
2021-09-08 21:55:51 +0300

    Completed Pie chart with dynamic data

--- src/app/admin_project/components/charts/pie-charts/pie-charts.component.ts
+++ src/app/admin_project/components/charts/pie-charts/pie-charts.component.ts
@@ -1,10 +1,24 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild} from '@angular/core';
 
-// @ts-ignore
-import * as CanvasJS from './canvasjs.min';
 import {ChartsService} from '../../../services/charts.service';
 import {Router} from '@angular/router';
-import {ChartUsersRolesDto} from "../../../models/chart-users-roles-dto.model";
+import {ChartUsersRolesDto} from '../../../models/chart-users-roles-dto.model';
+import {
+  ApexNonAxisChartSeries,
+  ApexResponsive,
+  ApexChart,
+  ApexTheme,
+  ApexTitleSubtitle, ChartComponent
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexNonAxisChartSeries | any;
+  chart: ApexChart | any;
+  responsive: ApexResponsive[] | any;
+  labels: any;
+  theme: ApexTheme | any;
+  title: ApexTitleSubtitle | any;
+};
 
 @Component({
   selector: 'app-pie-charts',
@@ -12,57 +26,63 @@ import {ChartUsersRolesDto} from "../../../models/chart-users-roles-dto.model";
   styleUrls: ['./pie-charts.component.css']
 })
 export class PieChartsComponent implements OnInit {
-  title = 'canvasjs-angular';
-  chartDataList: any[] = [];
-  chartData: ChartUsersRolesDto | any;
-  adminR: bigint;
-  studentR: number;
-  noRoles: number;
-  i: number = 6;
-  f: number = 10;
+  @ViewChild("chart") chart: ChartComponent;
+  chartDataList: ChartUsersRolesDto[] = [];
+  chartData: ChartUsersRolesDto;
+  public chartOptions: Partial<ChartOptions>;
 
   constructor(private chartService: ChartsService, private router: Router) {
-
   }
 
-
   ngOnInit(): void {
-    this.chartService.getUsersRolesChart().subscribe(
-      data => {
-        this.chartDataList = data;
-        for (let i = 0; this.chartDataList.length > i; i++) {
-          this.chartData = this.chartDataList[i];
-        }
-        this.adminR = <bigint>this.chartData.adminRoles;
-        this.studentR = parseInt(this.chartData.studentRoles);
-        this.noRoles = +this.chartData.noRoles;
-        console.log(this.adminR, this.studentR);
-      },
-      (error: any) => {console.log(error);
-      })
+    this.chartService.getUsersRolesChart()
+      .subscribe(
+    data => {
+      this.chartDataList = data;
+      for (let i = 0; this.chartDataList.length > i; i++) {
+        this.chartData = this.chartDataList[i];
+      }
+      let adminRoles = <number>this.chartData['adminRoles'];
+      let teacherRoles = <number>this.chartData['teacherRoles'];
+      let studentsRoles = <number>this.chartData['studentRoles'];
+      let noRoles = <number>this.chartData['noRoles'];
+      console.log(noRoles);
 
-    let dataPoints = [
-      { y: this.f },
-      { y: this.i },
-      { y: 50 },
-      { y: 65 }
-      // { y: this.adminR },
-      // { y: this.teacherR },
-      // { y: this.studentR },
-      // { y: this.noRoles }
 
-    ];
-
-    let chart = new CanvasJS.Chart("chartContainer",{
-      animationEnabled: true,
-      title:{
-        text: "Pie chart for Users Roles"
-      },
-      data: [{
-        type: "pie",
-        dataPoints : dataPoints
-      }]
-    });
-    chart.render();
+      this.chartOptions = {
+        series: [adminRoles, teacherRoles, studentsRoles, noRoles],
+        chart: {
+          width: "130%",
+          type: "pie"
+        },
+        labels: [
+          "Admins",
+          "Teachers",
+          "Students",
+          "Without any role"
+        ],
+        theme: {
+          monochrome: {
+            enabled: true
+          }
+        },
+        title: {
+          text: "Users Roles Chart"
+        },
+        responsive: [
+          {
+            breakpoint: 480,
+            options: {
+              chart: {
+                width: 200
+              },
+              legend: {
+                position: "bottom"
+              }
+            }
+          }
+        ]
+      };
+    })
   }
 }
