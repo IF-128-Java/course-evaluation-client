bb8fa87f5bb85f3368377bfe9d6d1ab49a940e54
Arsen-Kushnir <arsikkushnir@gmail.com>
2021-09-19 17:28:31 +0300

    notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,11 @@ import {Component, OnInit} from '@angular/core';
 import {TokenStorageService} from './auth/token-storage.service';
 import {MatIconRegistry} from '@angular/material/icon';
 import {DomSanitizer} from '@angular/platform-browser';
-
+import * as SockJS from "sockjs-client";
+import * as Stomp from "stompjs";
+import {AppConfig} from "./common/app-config";
+import {SiteNotificationService} from "./services/site-notification.service";
+import {SiteNotification} from "./models/site-notification.model";
 
 @Component({
   selector: 'app-root',
@@ -16,8 +20,14 @@ export class AppComponent implements OnInit {
   id: string | undefined;
   public isExpanded = false;
 
+  notifications: SiteNotification[] = [];
+  countUnreviewedNotifications: number = 0;
+  private stompClient: any;
 
-  constructor(private tokenStorage: TokenStorageService,private matIconRegistry: MatIconRegistry,private domSanitizer: DomSanitizer,) {
+  constructor(private tokenStorage: TokenStorageService,
+              private matIconRegistry: MatIconRegistry,
+              private domSanitizer: DomSanitizer,
+              private siteNotificationService: SiteNotificationService) {
     this.matIconRegistry.addSvgIcon('users',this.domSanitizer
       .bypassSecurityTrustResourceUrl('assets/images/admin-icons/users.svg'));
     this.matIconRegistry.addSvgIcon('students',this.domSanitizer
@@ -40,14 +50,52 @@ export class AppComponent implements OnInit {
       this.roles = this.tokenStorage.getAuthorities();
       this.username = this.tokenStorage.getUsername();
       this.id = this.tokenStorage.getId();
+      this.getSiteNotifications()
+      this.initializeWebSocketConnection();
     }
   }
 
   logout() {
+    this.stompClient.disconnect(()=>{}, {});
     this.tokenStorage.signOut();
     window.location.href= '/home';
   }
 
+  getSiteNotifications(){
+    this.siteNotificationService.getSiteNotifications().subscribe(data => {
+        this.notifications = data;
+        this.notifications.sort((el1, el2) => el2.createdAt);
+        this.countUnreviewedNotifications = this.notifications.filter((el) => !el.reviewed).length;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
 
+  initializeWebSocketConnection(){
+    let ws = new SockJS(AppConfig.API_ADMIN_ENDPOINT + 'connect-ws');
+    this.stompClient = Stomp.over(ws);
+    this.stompClient.connect({}, () => {
+      this.stompClient.subscribe('/api/v1/admin/event/site-notifications/users/' + this.id, (message: any) => {
+        if(message.body) {
+          let retrievedNotification = JSON.parse(message.body);
+          this.notifications.unshift(retrievedNotification);
+          this.countUnreviewedNotifications = this.notifications.filter((el) => !el.reviewed).length;
+        }
+      });
+    });
+  }
 
+  setReviewed(notification: SiteNotification) {
+    this.siteNotificationService.setReviewedSiteNotification(notification.id!).subscribe(() => {
+        let foundNotification = this.notifications.find(el => el.id == notification.id);
+        foundNotification!.reviewed = true;
+        this.countUnreviewedNotifications = this.notifications.filter((el) => !el.reviewed).length;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
 }
